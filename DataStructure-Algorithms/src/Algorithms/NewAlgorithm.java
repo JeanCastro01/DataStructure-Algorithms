7bc466ce93b23452df2ca24f9330cd7e75c7b494
jean <jeancastro.contato@gmail.com>
2022-03-13 20:06:06 +0000

    New Algorithms

---
+++ DataStructure-Algorithms/src/Algorithms/NewAlgorithm.java
@@ -0,0 +1,14 @@
+package Algorithms;
+
+public class NewAlgorithm {
+
+	public NewAlgorithm() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
